0cacf106b1c6a973267101249aa1351b20f814eb
Rommel Rico <me@rommelrico.com>
2018-11-14 05:29:09 -0800

    Unused import

--- app/src/main/java/com/rommelrico/memorableplacesandroid/MainActivity.kt
+++ app/src/main/java/com/rommelrico/memorableplacesandroid/MainActivity.kt
@@ -3,7 +3,6 @@ package com.rommelrico.memorableplacesandroid
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.ListView
 import java.util.ArrayList
