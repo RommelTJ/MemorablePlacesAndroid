067dc5e21530c3faacacd7bd20669441e2e93c6a
Rommel Rico <me@rommelrico.com>
2018-11-14 05:22:22 -0800

    onRequestPermissionsResult

--- app/src/main/java/com/rommelrico/memorableplacesandroid/MapsActivity.kt
+++ app/src/main/java/com/rommelrico/memorableplacesandroid/MapsActivity.kt
@@ -1,10 +1,13 @@
 package com.rommelrico.memorableplacesandroid
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.location.Location
 import android.location.LocationListener
 import android.location.LocationManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.content.ContextCompat
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -18,6 +21,18 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     internal var locationManager: LocationManager? = null
     internal var locationListener: LocationListener? = null
     private lateinit var mMap: GoogleMap
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                locationManager?.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0f, locationListener)
+                val lastKnownLocation = locationManager?.getLastKnownLocation(LocationManager.GPS_PROVIDER)
+                centerMapOnLocation(lastKnownLocation, "Your Location")
+            }
+        }
+    } // end onRequestPermissionsResult
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
